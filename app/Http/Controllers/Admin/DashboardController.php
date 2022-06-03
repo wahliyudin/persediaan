b2d43e91fe3b3e1dbb671196d130f9ae59e78c58
wahliyudin <wahliyudinzein@gmail.com>
2022-06-03 19:21:50 +0700

    Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\Warehouse;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -16,6 +19,11 @@ class DashboardController extends Controller
                     'Dashboard' => 0
                 ]
             ],
+            'total_barang_masuk' => Transaction::where('status', Transaction::STATUS_IN)->sum('jumlah'),
+            'total_barang_keluar' => Transaction::where('status', Transaction::STATUS_OUT)->sum('jumlah'),
+            'total_barang_semua_gudang' => Product::count(),
+            'products' => Product::with('unit', 'type', 'warehouse')->where('stock', '<', 20)->get(),
+            'warehouses' => Warehouse::withCount('products')->get()
         ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,14 +8,15 @@
                 <!-- small box -->
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3>150</h3>
+                        <h3>{{ $total_barang_masuk }}</h3>
 
                         <p>Barang Masuk</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-sign-in-alt"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('admin.incoming-product.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -23,14 +24,15 @@
                 <!-- small box -->
                 <div class="small-box bg-success">
                     <div class="inner">
-                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+                        <h3>{{ $total_barang_keluar }}</h3>
 
                         <p>Barang Keluar</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-sign-out-alt"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('admin.product-out.index') }}" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -38,20 +40,72 @@
                 <!-- small box -->
                 <div class="small-box bg-warning">
                     <div class="inner">
-                        <h3 style="color: white;">5</h3>
+                        <h3 style="color: white;">{{ $total_barang_semua_gudang }}</h3>
 
                         <p style="color: white;">Total Barang Semua Gudang</p>
                     </div>
                     <div class="icon">
                         <i class="fas fa-boxes"></i>
                     </div>
-                    <a href="" class="small-box-footer">Detail <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('admin.products.index') }}" class="small-box-footer"
+                        style="color: white !important;">Detail <i class="fas fa-arrow-circle-right"
+                            style="color: white;"></i></a>
                 </div>
             </div>
         </div>
         <!-- /.row -->
+        <div class="row">
+            <div class="col-lg-9">
+                <div class="card">
+                    <div class="card-body">
+                        <table id="product" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Jenis</th>
+                                    <th>Satuan</th>
+                                    <th>Gudang</th>
+                                    <th>Stok</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($products as $product)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $product->name }}</td>
+                                        <td>{{ $product->type->name }}</td>
+                                        <td>{{ $product->unit->name }}</td>
+                                        <td>{{ $product->warehouse->name }}</td>
+                                        <td>{{ $product->stock }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                @foreach ($warehouses as $warehouse)
+                    <div class="col-lg-12">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3 style="color: white;">{{ $warehouse->products_count }}</h3>
+
+                                <p style="color: white;">Total Barang {{ $warehouse->name }}</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-boxes"></i>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div><!-- /.container-fluid -->
 @endsection
+@include('layouts.inc.datatables')
 @push('css')
     <link rel="stylesheet" href="{{ asset('plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/toastr/toastr.min.css') }}">
@@ -59,4 +113,13 @@
 @push('script')
     <script src="{{ asset('plugins/sweetalert2/sweetalert2.min.js') }}"></script>
     <script src="{{ asset('plugins/toastr/toastr.min.js') }}"></script>
+    <script>
+        $(function() {
+            $("#product").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+            }).buttons().container().appendTo('#product_wrapper .col-md-6:eq(0)');
+        });
+    </script>
 @endpush
